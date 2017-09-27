ed9c9ced4034a1481381624057142338410a6c9a
Maciej Babiarz <maciej.babiarz@gmail.com>
2017-09-27 20:17:11 +0200

    5.2_product_create_method

--- objects/product.php
+++ objects/product.php
@@ -40,5 +40,36 @@ class Product
         return $stmt;
     }
 
+    // create product
+    public function create()
+    {
+        // query to insert product
+        $query = "INSERT INTO products 
+            SET name=:name, price=:price, description=:description, 
+            category_id=:category_id, created=:created";
 
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+
+        // sanitize
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->price = htmlspecialchars(strip_tags($this->price));
+        $this->description = htmlspecialchars(strip_tags($this->description));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+        $this->created = htmlspecialchars(strip_tags($this->created));
+
+        // bind values
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":price", $this->price);
+        $stmt->bindParam(":description", $this->description);
+        $stmt->bindParam(":category_id", $this->category_id);
+        $stmt->bindParam(":created", $this->created);
+
+        // execute quuery
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
